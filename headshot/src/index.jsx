40212b042bf4df5b086a7e41bf4fb0f1c164bc3a
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-02-22 15:50:29 +0500

    i ahve completed collapse button and complete 55% of my app and 2nd last div on page 1

--- headshot/src/index.jsx
+++ headshot/src/index.jsx
@@ -447,17 +447,19 @@ function App() {
                     </div>
                 </div>
 
+                <div className="getHeadshotDiv">
 
+                    <p>Get your headshots today!</p>
+                    <b>
+                        Your professional corporate business headshots are just a few clicks away. Don't miss out on the opportunity to showcase your team's best image - get your AI generated headshots now!
+                        create yours now
+                    </b>
+
+                  <a href="#">  <button className="getHeadshotDivBtn" >create yours now</button> </a>
+
+                </div>
 
             </div>
-
-
-
-
-
-
-
-
 
         </div >
     )
