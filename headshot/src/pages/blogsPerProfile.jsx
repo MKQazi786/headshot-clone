6846fb581ef3a3094b6e47a498579203397d5d6d
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-03-13 17:11:09 +0500

    i have completed perBlogProfile 70%

--- headshot/src/pages/blogsPerProfile.jsx
+++ headshot/src/pages/blogsPerProfile.jsx
@@ -25,7 +25,7 @@ const blogsPerProfile = () => {
 
             <main className="mainDiv" >
                 <img src={MP} alt="" width={1000} />
-                <p className="blogPara" >
+                <p>
                     Why do so many construction companies fail? According to the US Department
                     of Commerce, construction and contracting businesses have the highest failure
                     rate of any other business. Up to 96% of these companies fail before reaching
@@ -52,13 +52,96 @@ const blogsPerProfile = () => {
                     <li>Summary – Get Help</li>
                 </ol>
 
-                <h3>Insufficient Cash Flow</h3>
-                <p className="blogPara" >
+                <h3 className="iCF" >Insufficient Cash Flow</h3>
+                <p>
 
-                Cash flow is king. Without a constant positive cash flow, construction companies and speciality contractors are finding it difficult to keep their doors open for business. Whether that be because they are struggling to pay for materials, meeting payroll, keeping up with advertising demands, or any other number of reasons.
-                <br /> <br />
-                When cash flow is negative, companies often make hasty decisions and take on jobs that aren’t well suited to their companies needs. This can result in further profit loss and more significant cash flow problems down the road – causing companies to fail. But, how do you maintain positive cash flow? Keep reading…
-                </p> 
+                    Cash flow is king. Without a constant positive cash flow, construction companies and speciality contractors are finding it difficult to keep their doors open for business. Whether that be because they are struggling to pay for materials, meeting payroll, keeping up with advertising demands, or any other number of reasons.
+                    <br /> <br />
+                    When cash flow is negative, companies often make hasty decisions and take on jobs that aren’t well suited to their companies needs. This can result in further profit loss and more significant cash flow problems down the road – causing companies to fail. But, how do you maintain positive cash flow? Keep reading…
+                </p>
+
+                <h3>charging too little</h3>
+
+                <p>
+                    Most, if not all, struggling construction companies and specialty contractors do not charge enough for their services. Many companies don’t realize that they are charging too little because they don’t fully understand their overhead and operating expenses.
+                    <br /> <br />
+                    The billing price of each successful job must cover job costs, its share of overhead costs, plus a resonable profit. To build a sustainable construction company I recommend a minimun of 8% profit. Failing to charge enough on each job will soon lead to problem number one: insufficient cash flow.
+                </p>
+
+                <h3>lack of agreed upon payment schedule</h3>
+
+                <p className="blogPara" >Outlining a cash flow protective payment schedule on estimates and customer contracts will set a construction company on the road to success. A payment schedule that requires the company to finance parts of the job will consume cash flow. And in doing so, ultimately limit success. Instead, ensure that payment schedules cover, at minimun, job expenses and associated overhead.
+
+                    <br /> <br />
+                    In my experience, a 40% down payment on approved estimates and change orders works best. Have the customer follow up with weekly or biweekly progress payments that coincide with the percentage of completed work as outlined on the estimate. Customers rarely have a problem with well-documented and communicated payment schedules.
+
+                    <br /> <br />
+                    Communication and managing customer expectations is the key to success here.
+                </p>
+
+                <h3>insufficient number of profitable sales</h3>
+                <p>
+                    Many construction companies fail simply because they lack consistent and profitable jobs. For almost all businesses, advertising and one or more sales people are required to maintain a flow of consistent sales.
+                    <br /> <br />
+                    Additionally, a good website, search engine optimization (SEO), and a number of good online reviews will dramatically increase the flow of organic job leads. It’s common for construction companies to purchase job leads from companies like HomeAdvisor, Porch, Angies list, etc. Proper management of job leads and customer data will dramatically improve your ability to convert leads to sales.
+                    <br /><br />
+                    For this you’ll need a construction Customer Relationship Management (CRM) software solution. For obvious reasons, I recommend <i> Projul.</i> But in all honesty, it’s really the <i>Best construction CRM </i> and construction management system on the market for construction and specialty contractor businesses that want to make success easy.
+                </p>
+
+                <h3>lack of change orders</h3>
+                <p>
+                    Cha, cha, cha, changes… Unforeseen issues, customer scope creep, and upselling are just a few examples of why real time changes orders are needed on most construction and specialty contractor jobs. Waiting until the end of the job to tack on additional costs to the bill can be problematic. It will often result in customer sticker shock, bad reviews, and customer resistance to pay. Which in turn, leads to more cash flow problems and company failure.
+                    <br /><br />
+                    When a job change occurs, the construction company should submit a change order to the customer for approval. Companies should request a deposit on change orders that are over a couple thousand dollars. Customers can get excited about the work that is being done until the final bill comes. Then they find themsleves over extended and unable to pay.
+                    <br /><br />
+                    Having a signed change order and a deposit before the work begins ensures that the company and customer are on the same page. Ultimately improving cash flow and profitability. I highly recommend leaving very little money left on the table for the end of the job. Large payments due at the end of the project can be hard to collect.
+                    <br /><br />
+                    Take away – Make consistent job progress, communicate progress and manage expectations with the customer, and pull progress payments to match. By the time the job is finished the customer should owe you a relatively small amount.
+                </p>
+
+                <h3>lack of legal contract</h3>
+                <p>
+                    It’s essential to establish common ground and strong communciation between the company and customer. Creating a detailed estimate and contract for the customer to sign is imperative toward achieving this goal.
+                    <br /> <br />
+                    There are as many bad customers out there as there are bad contractors. Some of those customers simply want to get the work done for free. They will argue about things that, they claim, were promised. A signed contract and detailed estimate will help to prevent this from happening.
+                </p>
+
+                <h3>excess overhhead and employees</h3>
+                <p>
+                    Don’t get too excited to add employees. Company growth is what you want. But only add each employee only when absolutely necessary and after ensuring that the company can afford to do it. Employee payroll is one of a company’s biggest expenses. Do the math to ensure an additional employee will help the bottom line. Otherwise, it’ll consume it’s cash flow.
+                    <br /> <br />
+                    When you do hire an employee, hire slow and fire fast. A great empowered employee often has the productivity of three or more bad employees. A bad employee can quickly poison your construction company and demotivate an entire team. This is especially true for small to mid-size companies. Unfortunately, I learned that the hard way many times. It’s unprofitable to be in the business of fixing bad employees.
+                </p>
+
+                <h3>Lack of Organization, Processes & Decision Making Data</h3>
+                <p>
+                    Growing a sustainable construction business is impossible when the bulk of the business is operating out of one person’s head. In order to grow, you’ll need projects and issues that can be efficiently managed by the people you hire to help you. To do so, you’ll need more data transparency, processes, and organization.
+                    <br /> <br />
+                    For larger companies that use sticky notes and boxes full of folders, you essentially have the same problem on a bigger scale. Job leads and customer management, project details, task management, communications, schedules, progress tracking, equipment management, the list goes on and on.
+                    <br /> <br />
+                    Analyzing lead, advertising, sales, schedule, and project data from file folders and spreadsheets to make good business decisions is daunting. Each of these elements are extremely time consuming, error prone, and can negatively affect profitability and cash flow.
+                </p>
+
+                <h3><i>Summary: Get Help</i></h3>
+                <p>
+                    The best construction companies and contractors are using construction management software to grow, increase profits, and to help manage all of these. Now that you have a clear understanding why construction companies fail, it’s time to make some course corrections.
+                    <br /><br />
+                    There are a few decent construction management software products on the market that will make your construction life easier and more productive. <i>If you’re operating a small to mid-size construction company of 4 to 50 employees then of course I highly recommend Projul </i> construction management software.
+                    <br /><br />
+                    It will change your life, organize your business, simplify your scheduling, provide you with amazing reporting insights. Most importantly, it will increase your profit. If construction management software is not in your future, then I recommend diligently daily usage of spreadsheets to manage at minimum, leads and projects.
+                </p>
+                <div className="divRB" >
+
+                    <p className="paraRB">Recent Blogs</p>
+                    <p>
+                        Create professional portraits of the highest caliber for every need, from
+                        a simple business headshot, content marketing and even offline collateral.
+                    </p>
+                </div>
+
+                <div>
+                    
+                </div>
 
             </main>
         </>
