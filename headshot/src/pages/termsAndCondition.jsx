63122658fc819eb26a041975111120c951c39dd3
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-03-18 13:40:34 +0500

    i have start T&C page and added 4 terms

--- headshot/src/pages/termsAndCondition.jsx
+++ headshot/src/pages/termsAndCondition.jsx
@@ -1,10 +1,67 @@
 import React from "react";
 import Header from "../components/header";
+import "../cssPages/t&C.css";
+
 
 const TermsAndCondition = () => {
     return (
+        <div className="tAndC-Main" >
+            <Header />
+            <main className="mainDivTAC" >
+                <p>Terms & Conditions</p>
+                <b>
+                    THENEXTPHOTO and related services to you, a user, under the condition
+                    that you accept all of our set terms, including <br /> these  Terms of Service
+                    and all related policies.
+                </b>
+                <hr />
+                <hr />
 
-        <Header />
+                <div>
+                    <b>
+                        Thank you for using THENEXTPHOTO image generation and headshot photography services (the “Services”). These Terms of Service (the “Agreement”) spell out what rights you have with respect to the Service generated images, and other assets, which you generate (the “Assets”), your use of the Services, and other important topics like arbitration. Please read it carefully. We have a privacy policy which outlines how we handle your data . This Agreement is entered into by THENEXTPHOTO Inc. and the entity or person agreeing to these terms ("Customer") and govern Customer's access to and use of the Services.
+                        <br />
+                        <br />
+                        This Agreement is effective when the Customer is presented with this Agreement and proceeds to use the Services (the "Effective Date") or to receive or distribute Assets. These terms may be updated and presented again to the Customer from time to time. Continued use of the Services constitutes acceptance of the updated terms. If you do not agree to the Agreement, please stop using the Services.
+                        <br />
+                        <br />
+                        Other documents referenced here may also bind Customer’s use of this Services, including the Community Guidelines included below.
+                    </b>
+
+                    <h3>1. &nbsp; Service Availability and Quality</h3>
+                    <b>
+                        We are constantly evolving the Services to make them better for you. The Services are subject to modification and change, including but not limited to the art style of Assets, the algorithms used to generate the Assets, and features available to the Customer. No guarantees are made with respect to the Services’ quality, stability, uptime or reliability. Please do not create any dependencies on any attributes of the Services or the Assets. We will not be liable to you or your downstream customers for any harm caused by your dependency on the Service.
+                        <br /> <br />
+                        Both the Services and the Assets are provided to Customer on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Assets and assume any risks associated with use of the Services.
+                        <br /> <br />
+                        Reserves the right to suspend or ban your access to the Services at any time, and for any reason. You may not use the Services for competitive research. You may not reverse engineer the Services or the Assets. You may not use automated tools to access, interact with, or generate Assets through the Services. Only one user may use the Services per registered account. Each user of the Services may only have one account. We reserve the right to investigate complaints or reported violations of our Terms of Service and to take any action we deem appropriate including but not limited to reporting any suspected unlawful activity to law enforcement officials, regulators, or other third parties and disclosing any information necessary or appropriate to such persons or entities relating to user profiles, e-mail addresses, usage history, posted materials, IP addresses and traffic information.
+                    </b>
+
+                    <h3>2. &nbsp; Age requirement</h3>
+                    <b>
+                        By accessing the Services, you confirm that you’re at least 13 years old and meet the minimum age of digital consent in your country. If you are old enough to access the Services in your country, but not old enough to have authority to consent to our terms, your parent or guardian must agree to our terms on your behalf. Please ask your parent or guardian to read these terms with you. If you’re a parent or legal guardian, and you allow your teenager to use the Services, then these terms also apply to you and you’re responsible for your teenager’s activity on the Services. THENEXTPHOTO  tries to make its Services PG-13 and family friendly, but the Assets are generated by an artificial intelligence system based on user queries. This is new technology and it does not always work as expected. No guarantees are made as to the suitability of the Assets for the Customer.
+                    </b>
+
+                    <h3>3. &nbsp; your information</h3>
+                    <b>
+                        By using the Services, you may provide THENEXTPHOTO with personal information like your email address, user name, billing information, favorites, image outputs, and text prompts that you enter, or sample images that you upload to the Service. Our policy outlining what we do with your personal information, and the rights you have with respect to your personal information, can be found . Both parties agree to comply with all applicable data protection laws. To the extent the parties need to enter into additional terms regarding data privacy or data transfers the parties will work together in good faith to enter into such terms.
+                    </b>
+
+                    <h3>4. &nbsp; copyright and trademark</h3>
+                    <b>
+                        To the extent allowed by law and as between you and THENEXTPHOTO, you own your Uploads, and you agree that THENEXTPHOTO owns all Generations, and you hereby make any necessary assignments for this. THENEXTPHOTO grants you the exclusive rights to reproduce and display such Generations and will not resell Generations that you have created, use Generations in any way, or assert any copyright in such Generations against you or your end users, all provided that you comply with these terms and our Content Policy. If you violate our terms or Content Policy, you will lose rights to use Generations, but we will provide you written notice and a reasonable opportunity to fix your violation, unless it was clearly illegal or abusive. You understand and acknowledge that similar or identical Generations may be created by other people using their own Prompts, and your rights are only to the specific Generation that you have created.
+                        <br /> <br />
+                        Both the Services and the Assets are provided to Customer on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Assets and assume any risks associated with use of the Services.
+                        <br /> <br />
+                        Reserves the right to suspend or ban your access to the Services at any time, and for any reason. You may not use the Services for competitive research. You may not reverse engineer the Services or the Assets. You may not use automated tools to access, interact with, or generate Assets through the Services. Only one user may use the Services per registered account. Each user of the Services may only have one account. We reserve the right to investigate complaints or reported violations of our Terms of Service and to take any action we deem appropriate including but not limited to reporting any suspected unlawful activity to law enforcement officials, regulators, or other third parties and disclosing any information necessary or appropriate to such persons or entities relating to user profiles, e-mail addresses, usage history, posted materials, IP addresses and traffic information.
+                    </b>
+
+
+                </div>
+
+
+            </main>
+        </div>
 
     )
 }
