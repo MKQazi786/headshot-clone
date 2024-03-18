78499d3ef71e8dca3b2ec7e3552aa1e9f92f75b3
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-03-18 16:36:32 +0500

    i have completed my all pages in this react app

--- headshot/src/pages/termsAndCondition.jsx
+++ headshot/src/pages/termsAndCondition.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/header";
+import Footer from "../components/footer";
 import "../cssPages/t&C.css";
 
 
@@ -56,12 +57,133 @@ const TermsAndCondition = () => {
                         Reserves the right to suspend or ban your access to the Services at any time, and for any reason. You may not use the Services for competitive research. You may not reverse engineer the Services or the Assets. You may not use automated tools to access, interact with, or generate Assets through the Services. Only one user may use the Services per registered account. Each user of the Services may only have one account. We reserve the right to investigate complaints or reported violations of our Terms of Service and to take any action we deem appropriate including but not limited to reporting any suspected unlawful activity to law enforcement officials, regulators, or other third parties and disclosing any information necessary or appropriate to such persons or entities relating to user profiles, e-mail addresses, usage history, posted materials, IP addresses and traffic information.
                     </b>
 
+                    <h3>5. &nbsp; DMCA and Takedowns Policy</h3>
+                    <b>
+                        THENEXTPHOTO uses an artificial intelligence system trained on public datasets to produce its Assets. Such Assets may be unintentionally similar to copyright protected material or trademarks you hold. We respect rights holders internationally. If you believe your copyright or trademark is being infringed by the Service, please write to takedown@thenextphoto.com and we will process your request.
+                    </b>
 
+                    <h3>6. &nbsp; Dispute Resolution and Governing Law</h3>
+                    <b>
+                        <ol>
+                            <li>
+                                ALL LEGAL CLAIMS ARISING OUT OF OR RELATING TO THIS AGREEMENT (INCLUDING ANY DISPUTE REGARDING THE INTERPRETATION OR PERFORMANCE OF THE AGREEMENT) ("Dispute") WILL BE GOVERNED BY THE LAWS OF COUNTRY OF SINGAPORE
+                            </li>
+
+                            <li>
+                                The parties will try in good faith to settle any Dispute within 30 days after the Dispute arises. If the Dispute is not resolved within 30 days, it shall be resolved by arbitration by the Singaporean Arbitration Association’s International Centre for Dispute Resolution in accordance with its Expedited Commercial Rules in force as of the date of this Agreement ("Rules").
+                            </li>
+
+                            <li>
+                                The parties will mutually select one arbitrator. The arbitration will be conducted in English in Singapore.
+                            </li>
+
+                            <li>
+                                Either party may apply to any competent court for injunctive relief necessary to protect its rights pending resolution of the arbitration. The arbitrator may order equitable or injunctive relief consistent with the remedies and limitations in the Agreement.
+                            </li>
+
+                            <li>
+                                The arbitral award will be final and binding on the parties and its execution may be presented in any competent court, including any court with jurisdiction over either party or any of its property.
+                            </li>
+
+                            <li>
+                                Each party will bear its own lawyers’ and experts’ fees and expenses, regardless of the arbitrator’s final decision regarding the Dispute.
+                            </li>
+                        </ol>
+                    </b>
+
+                    <h3>7. &nbsp;ownership</h3>
+                    <ol>
+                        <li>
+                            Customer Ownership: By using our services, you retain full ownership of all photos and data that you upload, store, or transmit through our platform. We do not claim any rights or ownership over your photos or data.
+                        </li>
+
+                        <li>
+                            Non-Usage Without Explicit Consent: We are committed to respecting your privacy and ensuring the security of your photos and data. We will not use, sell, rent, or otherwise exploit your photos or data without obtaining your explicit consent.
+
+                        </li>
+                        <li>
+                            Consent for Specific Purposes: In certain situations, we may request your explicit consent to use your photos or data for specific purposes that are outlined in separate agreements, features, or functionalities within our services. Such consent will be obtained separately, and we will only use your photos or data in accordance with the terms of the specific consent provided.
+                        </li>
+                        <li>
+                            Limited Data Processing: We may process your photos and data for the sole purpose of providing our services to you, including but not limited to storage, retrieval, backup, and data analysis, in accordance with applicable privacy laws and regulations.
+                        </li>
+
+                        <li>
+                            Data Security Measures: We employ reasonable security measures to protect your photos and data from unauthorized access, disclosure, alteration, or destruction. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+                        </li>
+
+                        <li>
+                            Third-Party Access: We will not provide access to your photos or data to any third parties, except as required by law, to comply with legal obligations, enforce our rights, prevent fraud, or protect the rights, property, or safety of our users.
+                        </li>
+
+
+                        <li>
+                            Data Deletion: Any data will automatically be deleted 30 days after usage. Upon your request or termination of your account, we will delete your photos and data from our active databases, subject to any legal obligations or technical limitations that may prevent us from doing so. Please note that residual copies of your photos or data may remain in our backup systems for a limited period of time.
+                        </li>
+
+                    </ol>
+
+                    <h3>8.&nbsp; payment and billing</h3>
+                    <b>
+                        We may invoice you for your use of the Services through a third party payment service provider. The third party service provider’s terms of service shall govern and supersede this Agreement in case of conflict. You are free to cancel your plan at any time. We also reserve the right to terminate your access to the Service for any reason, including for violation of the Community Guidelines or other inappropriate use of the Service. Upon cancellation or termination, any outstanding usage fees will be charged immediately. You will not be refunded for the current pay period, but you will stop being charging after the current pay period has ended. Refunds requested within 14 days of purchase are eligable for a full refund if product has not been used and no AI has been trained. We will withhold 10% of sales price for refunds. Any shoots with more than two downloads will also not be eligable for refunds.
+                    </b>
+
+                    <h3>9.&nbsp; community guidlines</h3>
+                    <ol>
+                        <li>
+                            Be kind and respect each other and staff. Do not create images or use text prompts that are inherently disrespectful, aggressive, or otherwise abusive. Violence or harassment of any kind will not be tolerated.
+                        </li>
+
+                        <li>
+                            No adult content or gore. Please avoid making visually shocking or disturbing content. We will block some text inputs automatically.
+                        </li>
+
+                        <li>
+                            Sharing others’ creations. Do not publicly repost the creations of others without their permission.
+                        </li>
+
+                        <li>
+                            Be careful about sharing. It’s OK to share your creations outside of the THENEXTPHOTO community but please consider how others might view your content.
+                        </li>
+
+                        <li>
+                            Any violations of these rules may lead to bans from our services.
+                        </li>
+
+                    </ol>
+
+                    <h3>10. &nbsp;Limitation of Liability and Indemnity</h3>
+                    <b>
+                        We provide the service as is, and we make no promises or guarantees about it. You understand and agree that we will not be liable to you or any third party for any loss of profits, use, goodwill, or data, or for any incidental, indirect, special, consequential or exemplary damages, however they arise. You are responsible for your use of the service. If you harm someone else or get into a dispute with someone else, we will not be involved. If you knowingly infringe someone else’s intellectual property, and that costs us money, we’re going to come find you and collect that money from you. We might also do other stuff, like try to get a court to make you pay our attorney’s fees. Don’t do it.
+                    </b>
+
+                    <h3>11. &nbsp;miscellaneous</h3>
+                    <ol>
+                        <li>
+                            Force Majeure. Neither party will be liable for failure or delay in performance to the extent caused by circumstances beyond its reasonable control, including acts of God, natural disasters, terrorism, riots, or war.
+                        </li>
+
+                        <li>
+                            No Agency. This Agreement does not create any agency, partnership, or joint venture between the parties.
+                        </li>
+
+                        <li>
+                            Severability. If any part of this Agreement is invalid, illegal, or unenforceable, the rest of the Agreement will remain in effect.
+                        </li>
+
+                        <li>
+                            No Third-Party Beneficiaries. This Agreement does not confer any benefits on any third party unless it expressly states that it does.
+                        </li>
+
+                        <li>
+                            Survival. The sections and obligations in this Agreement that a reasonable person would expect to survive this agreement, will. Particularly the IP and privacy stuff.
+                        </li>
+                    </ol>
                 </div>
+            </main >
 
-
-            </main>
-        </div>
+            <Footer />
+        </div >
 
     )
 }
